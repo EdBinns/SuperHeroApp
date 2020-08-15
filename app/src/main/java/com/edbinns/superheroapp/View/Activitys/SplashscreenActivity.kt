c8c065412f923de73172309bf8c57f6de2b57dae
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-08-14 18:02:17 -0600

    Primer commit

---
+++ app/src/main/java/com/edbinns/superheroapp/View/Activitys/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.edbinns.superheroapp.View.Activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edbinns.superheroapp.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
---
+++ app/src/main/java/com/edbinns/superheroapp/View/Fragments/FavoriteHeroFragment.kt
@@ -0,0 +1,23 @@
+package com.edbinns.superheroapp.View.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.edbinns.superheroapp.R
+
+
+class FavoriteHeroFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_hero, container, false)
+    }
+
+}
