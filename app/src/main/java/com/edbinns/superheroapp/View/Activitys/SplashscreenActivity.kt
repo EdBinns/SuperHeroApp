d1d2d49a22b20860b9205a8432377a4ad8690783
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-08-15 21:43:54 -0600

    Detail dialog hero terminado, detail dialog comic terminado y aplshscreen terminado

--- app/src/main/java/com/edbinns/superheroapp/View/Activitys/SplashscreenActivity.kt
+++ app/src/main/java/com/edbinns/superheroapp/View/Activitys/SplashscreenActivity.kt
@@ -1,12 +1,39 @@
 package com.edbinns.superheroapp.View.Activitys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.edbinns.superheroapp.R
+import kotlinx.android.synthetic.main.activity_splashscreen.*
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        val animacion = AnimationUtils.loadAnimation(this, R.anim.anim_splashscreen)
+        imageSplash.startAnimation(animacion)
+
+        val intent = Intent(this, LogInActivity::class.java)
+
+        animacion.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+        })
+
     }
+
+
 }
