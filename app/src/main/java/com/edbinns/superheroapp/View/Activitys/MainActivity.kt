0f9c622f0bff496f7abea92ca904f2de84accb5f
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-08-21 14:53:11 -0600

    BottomNavigation arreglado

--- app/src/main/java/com/edbinns/superheroapp/View/Activitys/MainActivity.kt
+++ app/src/main/java/com/edbinns/superheroapp/View/Activitys/MainActivity.kt
@@ -2,11 +2,21 @@ package com.edbinns.superheroapp.View.Activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.edbinns.superheroapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setActionBar(findViewById(R.id.toolbarMain))
+        configNav()
+    }
+
+    private fun configNav() {
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragContent))
     }
 }
