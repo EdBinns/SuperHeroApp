994e379447595c5303d281bb7b8a378afec1582a
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-08-20 21:15:27 -0600

    Cmabio leves en vistas

--- app/src/main/java/com/edbinns/superheroapp/View/UI/Activitys/MainActivity.kt
+++ app/src/main/java/com/edbinns/superheroapp/View/UI/Activitys/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setActionBar(findViewById(R.id.toolbarMain))
         configNav()
 
         val bundle = intent.extras
--- app/src/main/java/com/edbinns/superheroapp/View/UI/Fragments/SuperHeroFragment.kt
+++ app/src/main/java/com/edbinns/superheroapp/View/UI/Fragments/SuperHeroFragment.kt
@@ -9,11 +9,6 @@ import com.edbinns.superheroapp.R
 
 class SuperHeroFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_super_hero, container, false)
